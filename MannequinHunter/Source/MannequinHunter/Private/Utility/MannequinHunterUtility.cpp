5ba5dc6daa5a9c1c1a9d2e071b77a2e77dd96fc6
OJK <dhwnsrud98@gmail.com>
2023-12-29 18:02:12 +0900

    로그 추가

--- MannequinHunter/Source/MannequinHunter/Private/Utility/MannequinHunterUtility.cpp
+++ MannequinHunter/Source/MannequinHunter/Private/Utility/MannequinHunterUtility.cpp
@@ -8,8 +8,13 @@ const float FMannequinHunterUtility::PLAY_RATE = 1.0f;
 
 float FMannequinHunterUtility::GetPlayRate(int32 characterAttackSpeed)
 {
+	//https://www.desmos.com/calculator?lang=ko 그래프 사이트
+	// 100 이상
+	// y = 1.225^x + (1 - 1.225)
+	// 100 미만
+	// sin(x*0.5PI)
 	float speed = characterAttackSpeed * 0.01f;
-	if (characterAttackSpeed > 100)
+	if (characterAttackSpeed >= 100)
 		return FMath::Pow(PLAY_RATE_CONSTANT_NUMBER, speed) + (1.0f - PLAY_RATE_CONSTANT_NUMBER);
 	else
 		return FMath::Sin(speed * 0.5 * UE_PI);
